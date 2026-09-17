8f8f9230d0ad8b0f0b6122a1e45337db3a920edb
Eborm <bramwesel@gmail.com>
2026-09-17 23:54:34 +0200

    Added comments to the main.tf in /talos-config. A little sloppy but its fine. Next on the agenda is to write the documentation so i can rebuild this from scratch if needed like i did with the creation of the vm's

--- talos-config/main.tf
+++ talos-config/main.tf
@@ -28,28 +28,30 @@ locals {
 //Generate talos machine secrets
 resource "talos_machine_secrets" "this" {}
 
+//Create the config for the talos control plane nodes. 
 data "talos_machine_configuration" "controlplane" {
-  cluster_name     = local.cluster_name
-  cluster_endpoint = local.cluster_endpoint
+  cluster_name     = local.cluster_name // Cluster name
+  cluster_endpoint = local.cluster_endpoint // cluster endpoint. --> Currently cp-01 (control plane node 1)
 
-  machine_type = "controlplane"
+  machine_type = "controlplane" //Set the machine type to controlplane
 
-  machine_secrets = talos_machine_secrets.this.machine_secrets
+  machine_secrets = talos_machine_secrets.this.machine_secrets //Use the machine secrets generated to create or join the cluster
 
-  talos_version = "v1.13.0"
+  talos_version = "v1.13.0" //Specify talos version
 
   config_patches = [
     yamlencode({
       machine = {
         install = {
-          disk = "/dev/sda"
-          image = "factory.talos.dev/nocloud-installer/ce4c980550dd2ab1b17bbf2b08801c7eb59418eafe8f279833297925d67c7515:v1.13.0"
+          disk = "/dev/sda" //Specify installation disk
+          image = "factory.talos.dev/nocloud-installer/ce4c980550dd2ab1b17bbf2b08801c7eb59418eafe8f279833297925d67c7515:v1.13.0" //No-cloud talos version 1.13.0 with qemu guest agent extension installed
         }
       }
     })
   ]
 }
 
+//Apply the controlplane configuration to all controlplane nodes
 resource "talos_machine_configuration_apply" "controlplane" {
   for_each = var.control_nodes
   node     = each.value.ip_address
@@ -59,6 +61,7 @@ resource "talos_machine_configuration_apply" "controlplane" {
   machine_configuration_input = data.talos_machine_configuration.controlplane.machine_configuration
 }
 
+//Start kubernetes contorlplane node at cp-01
 resource "talos_machine_bootstrap" "this" {
     node = var.control_nodes["cp-01"].ip_address
 
@@ -69,6 +72,7 @@ resource "talos_machine_bootstrap" "this" {
     ]
 }
 
+//Retrieves the kube config for the talos cluster. Unsure if this actually does what i just wrote but thats what the documentation says i think.
 resource "talos_cluster_kubeconfig" "this" {
   node = var.control_nodes["cp-01"].ip_address
 
@@ -79,14 +83,16 @@ resource "talos_cluster_kubeconfig" "this" {
   ]
 }
 
+//Create the configuration for workerplanes
 data "talos_machine_configuration" "worker" {
-  cluster_name     = local.cluster_name
-  cluster_endpoint = local.cluster_endpoint
+  cluster_name     = local.cluster_name //Clustername
+  cluster_endpoint = local.cluster_endpoint //Clusterendpoint currently cp-01
 
-  machine_type = "worker"
+  machine_type = "worker" //Specify machine type to be workerplane
 
   machine_secrets = talos_machine_secrets.this.machine_secrets
 
+  //Its the same as the controlplane its nothing special
   config_patches = [
     yamlencode({
       machine = {
@@ -99,6 +105,7 @@ data "talos_machine_configuration" "worker" {
   ]
 }
 
+//Apply workerplane configuration to worker nodes
 resource "talos_machine_configuration_apply" "worker" {
   for_each = var.worker_nodes
 
@@ -111,8 +118,4 @@ resource "talos_machine_configuration_apply" "worker" {
   depends_on = [
     talos_machine_bootstrap.this
   ]
-}
-
-output "worker_ips_debug" {
-  value = local.worker_ips
 }
